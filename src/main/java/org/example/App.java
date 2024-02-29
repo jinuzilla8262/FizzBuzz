b313db43765763b572b42347a24bc1f48751a9de
jinuz <jinuzilla@gmail.com>
2024-02-28 19:34:39 -0500

    Created Test Case for  A number is not divisible by ‘3’ or ‘5’ return the number itself (as a String) and it worked as we refactored it with refactoring last time.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,10 +9,9 @@ public class App
     public static void main( String[] args )
     {
 
-//        for(int i=0;i<100;i++){
-//            fizzBuzz(i);
-//            System.out.println(i + " :" +fizzBuzz(i));
-//        }
+        for(int i=0;i<100;i++){
+            System.out.println(i + " :" +fizzBuzz(i));
+        }
     }
     public static String fizzBuzz(int input) {
         String output = "";
